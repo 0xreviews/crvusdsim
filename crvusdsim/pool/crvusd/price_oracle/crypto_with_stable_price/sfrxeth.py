712c1d80a6584ee3ec8f2aa11d939a945b1d0403
Thomas Cintra <tncintra@gmail.com>
2024-01-20 17:27:15 -0500

    Add chainlink limits

--- crvusdsim/pool/crvusd/price_oracle/crypto_with_stable_price/sfrxeth.py
+++ crvusdsim/pool/crvusd/price_oracle/crypto_with_stable_price/sfrxeth.py
@@ -1,6 +1,6 @@
 """Provides the sfrxETH price oracle."""
 from typing import List
-from .base import Oracle, StakedOracle
+from .base import Oracle, SimpleOracle
 
 PRECISION = 10**18
 
@@ -8,9 +8,9 @@ PRECISION = 10**18
 class OracleSFRXETH(Oracle):
     """Oracle for sfrxETH"""
 
-    def __init__(self, staked_oracle: StakedOracle | None = None, **kwargs):
+    def __init__(self, staked_oracle: SimpleOracle | None = None, **kwargs):
         super().__init__(**kwargs)
-        self.staked_oracle = staked_oracle or StakedOracle()
+        self.staked_oracle = staked_oracle or SimpleOracle()
 
     def _raw_price(self, tvls: List[int], agg_price: int, *args, **kwargs) -> int:
         """
@@ -19,6 +19,9 @@ class OracleSFRXETH(Oracle):
 
         Convert this price to the staked asset price using staked oracle price.
         (e.g. ETH -> sfrxETH).
+
+        NOTE: don't need to apply chainlink limit here since the staked_oracle
+        price is already the simulated (i.e. market) price.
         """
         crv_p = super()._raw_price(tvls, agg_price, *args, **kwargs)
         crv_p = int(self.staked_oracle.price * crv_p // PRECISION)
--- test/oracle/test_crypto_with_stable_price.py
+++ test/oracle/test_crypto_with_stable_price.py
@@ -3,6 +3,7 @@ from hypothesis import given
 from hypothesis import strategies as st
 from ..utils import increment_timestamps
 from ..conftest import create_crypto_with_stable_price_oracle
+from test.utils import approx
 
 def trade(oracle, pool, i, j, frac):
     amount = pool.get_max_trade_size(i, j, frac)
@@ -55,3 +56,25 @@ def test_price_change_tricrypto(frac, market):
     # Raise collateral price
     price3 = trade(oracle, oracle.tricrypto[i], 0, ix, frac)
     assert price3 > price2
+
+@given(
+    frac=st.floats(min_value=0.1, max_value=0.9),
+    market=st.sampled_from(["weth", "wbtc", "wsteth", "sfrxeth"]),
+)
+def test_chainlink_limits(frac, market):
+    oracle = create_crypto_with_stable_price_oracle(market)
+
+    price1 = oracle.price_w()
+
+    oracle.set_chainlink(price1, 18, 1)  # tiny bounds
+
+    i = random.randint(0, len(oracle.tricrypto) - 1)
+    ix = oracle.tricrypto_ix[i] + 1
+
+    # Lower collateral price, ensure chainlink limits are respected
+    price2 = trade(oracle, oracle.tricrypto[i], ix, 0, frac)
+    assert approx(price2, price1, 1e-8)
+
+    # Raise collateral price, ensure chainlink limits are respected
+    price3 = trade(oracle, oracle.tricrypto[i], 0, ix, frac)
+    assert approx(price3, price1, 1e-8)
